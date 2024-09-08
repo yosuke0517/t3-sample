905373055e3b974e614fd815bf515c3690fe8e86
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2024-09-08 15:18:29 +0900

    fix: ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center">
         {session ? (
           <div>
             <p>server component user: {session.user.name}</p>
