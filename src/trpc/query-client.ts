46bdf7b6361d20660e6771165d2cb9bd8c77813a
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2024-09-10 20:26:27 +0900

    fix: server componentでもqueryClientを使えるように関数を移動

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -25,3 +25,14 @@ export const createQueryClient = () =>
       },
     },
   });
+
+// NOTE: サーバコンポーネントでも使いたいのでreact.tsxから移動
+let clientQueryClientSingleton: QueryClient | undefined = undefined;
+export const getQueryClient = () => {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return createQueryClient();
+  }
+  // Browser: use singleton pattern to keep the same query client
+  return (clientQueryClientSingleton ??= createQueryClient());
+};
