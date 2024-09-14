8ba985d77c9f3ce67c191721856b86995345f1ef
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2024-09-14 16:48:50 +0900

    fix: 一旦ログイン不要に

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,22 +2,22 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  // 開発環境と本番環境で異なるクッキー名を確認
-  const sessionCookie =
-    req.cookies.get("next-auth.session-token") ||
-    req.cookies.get("__Secure-next-auth.session-token");
-
-  // トークンが存在しない場合、ログインページにリダイレクト
-  if (!sessionCookie && req.nextUrl.pathname.startsWith("/post")) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  // ルート ("/") にアクセスしたときに /post にリダイレクト
-  if (req.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/post", req.url));
-  }
-
-  return NextResponse.next();
+  // // 開発環境と本番環境で異なるクッキー名を確認
+  // const sessionCookie =
+  //   req.cookies.get("next-auth.session-token") ||
+  //   req.cookies.get("__Secure-next-auth.session-token");
+  //
+  // // トークンが存在しない場合、ログインページにリダイレクト
+  // if (!sessionCookie && req.nextUrl.pathname.startsWith("/post")) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
+  //
+  // // ルート ("/") にアクセスしたときに /post にリダイレクト
+  // if (req.nextUrl.pathname === "/") {
+  //   return NextResponse.redirect(new URL("/post", req.url));
+  // }
+  //
+  // return NextResponse.next();
 }
 
 // ミドルウェアを適用するパス
